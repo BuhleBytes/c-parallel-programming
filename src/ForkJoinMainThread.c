70e56977dbf5db8fe5e2db54e6ed441efec095a9
Buhle Mlandu <146781698+BuhleBytes@users.noreply.github.com>
2025-05-19 01:10:44 +0200

    Added ForkJoin code were only the master thread gets to execute a thread

---
+++ src/ForkJoinMainThread.c
@@ -0,0 +1,21 @@
+#include <omp.h> //Include OMP library
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("\n 'Lets get to work' says thread %d\n", omp_get_thread_num());
+
+// Fork
+#pragma omp parallel
+    {
+        printf("Hello #1 from thread = %d\n", omp_get_thread_num());
+#pragma omp master
+        {
+            printf("\n'Lets pause' says thread %d\n\n", omp_get_thread_num());
+            printf("\n'I want a rest' says thread %d\n\n", omp_get_thread_num());
+        }
+        printf("Hello #2 from thread = %d\n", omp_get_thread_num());
+    } // Join
+    printf("\nAll done %d\n", omp_get_thread_num());
+    return 0;
+}
